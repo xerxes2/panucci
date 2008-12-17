31ce12dc728a1b15d2b598e1ba5474d39accd426
nikosapi <nikosapi@b83bf47a-295c-422b-8be9-b3ce96edbbb2>
2008-12-17 05:41:48 +0000

    Forgot to "svn add" the settings manager.
    I think it's time to get some sleep...
    
    
    git-svn-id: https://garage.maemo.org/svn/panucci/trunk@72 b83bf47a-295c-422b-8be9-b3ce96edbbb2

---
+++ src/panucci/settings.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2008 The Panucci Audiobook and Podcast Player Project
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+from simplegconf import gconf
+from util import log
+
+# If you add a setting _please_ update the schemas file
+
+default_settings = {
+    'db_location'       : '~/.panucci.sqlite',
+    'last_folder'       : '~',
+    'max_recent_files'  : 10,
+    'progress_locked'   : False,
+    'temp_playlist'     : '~/.panucci.m3u',
+    'volume'            : 0.3,
+}
+
+class Settings(object):
+    def __getattr__(self, key):
+        if default_settings.has_key(key):
+            return gconf.sget( key, default=default_settings[key] )
+        else:
+            log('Setting "%s" doesn\'t exist.' % key)
+
+    def __setattr__(self, key, value):
+        if default_settings.has_key(key):
+            gconf.sset( key, value )
+        else:
+            log('Setting "%s" doesn\'t exist.' % key)
+
+settings = Settings()
+
