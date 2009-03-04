acf042ba8420e86cd988e0e3d35d84f37ed41041
nikosapi <me@nikosapi.org>
2009-03-04 18:19:54 -0500

    Add notification support to the settings manager

--- src/panucci/settings.py
+++ src/panucci/settings.py
@@ -18,7 +18,12 @@
 #
 
 import logging
+import os.path
+
 from simplegconf import gconf
+from services import ObservableService
+
+SIGNAL_NAME_SUFFIX = '_changed'
 
 # If you add a setting _please_ update the schemas file
 
@@ -35,29 +40,59 @@ default_settings = {
     'volume'               : 0.3,
 }
 
-class Settings(object):
-    __log = logging.getLogger('panucci.settings.Settings')
+class Settings(ObservableService):
+    """ A settings manager for Panucci. A signal is emitted when any 
+        setting is changed; signal names are devised as follows:
+            setting_name + SIGNAL_NAME_SUFFIX 
+
+        The signal prototype is: def callback( new_value )"""
+
+    def __init__(self):
+        self.__log = logging.getLogger('panucci.settings.Settings')
+        signals = [ k + SIGNAL_NAME_SUFFIX for k in default_settings.keys() ]
+        ObservableService.__init__( self, signals, log=self.__log )
+
+        gconf.snotify( self.__on_directory_changed )
 
     def __getattr__(self, key):
         if default_settings.has_key(key):
             return gconf.sget( key, default=default_settings[key] )
         else:
             self.__log.warning('Setting "%s" doesn\'t exist.' % key)
+            raise AttributeError
 
     def __setattr__(self, key, value):
         if default_settings.has_key(key):
             if type(value) == type(default_settings[key]):
-                gconf.sset( key, value )
+                # Don't set the value if it's the same as it used to be
+                if getattr(self, key) != value:
+                    gconf.sset( key, value )
+
                 return True
             else:
                 self.__log.warning(
                     'Type of "%s" (%s) does not match default type (%s)',
-                    key, type(value), type(default_settings[key]) )
+                    key, type(value).__name__,
+                    type(default_settings[key]).__name__ )
         else:
+            object.__setattr__( self, key, value )
             self.__log.warning('Setting "%s" doesn\'t exist.', key)
 
         return False
 
+    def __on_directory_changed(self, client, connection_id, entry, args):
+        directory, key = os.path.split(entry.get_key())
+        new_value = getattr(self, key)
+        self.__log.debug('gconf key %s changed to: %s', key, new_value)
+        self.notify( key + SIGNAL_NAME_SUFFIX, new_value,
+            caller=self.__on_directory_changed )
+
+    def attach_checkbutton(self, button, setting):
+        button.connect(
+            'toggled', lambda w: setattr( self, setting, w.get_active()) )
+        self.register(
+            setting + SIGNAL_NAME_SUFFIX, lambda v: button.set_active(v) )
+        button.set_active( getattr(self, setting) )
 
 settings = Settings()
 
