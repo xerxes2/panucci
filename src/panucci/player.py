xerxes2/panucci

commit ee04fd56dd903312ecfccdb565f0499c2d4a9d8f

Make the player better integrated with the playlist
Now if the current_item_position is changed the player will change
  track on it's own. This means that the GUI no longer needs to
  call stop()/play() when jumping to a new track.

It also facilitates jumping to the next track if the current track is removed.

git-svn-id: https://garage.maemo.org/svn/panucci/trunk@126 b83bf47a-295c-422b-8be9-b3ce96edbbb2

src/panucci/player.py
@@ -42,6 +42,8 @@ class panucciPlayer(ObservableService):
         interface.register_player(self)
 
         self.playlist = Playlist()
+        self.playlist.register( 'new_track', self.on_new_track )
+
         self.__initial_seek = False # have we preformed the initial seek?
         self.seeking = False        # are we seeking?
 
@@ -240,16 +242,17 @@ class panucciPlayer(ObservableService):
         self.seeking = False
         return not error
 
+    def on_new_track(self):
+        self.stop(save_resume_point=False)
+        self.play()
+
     def __on_message(self, bus, message):
         t = message.type
         # self.__log.debug('Got message of type %s', t)
 
-        if t == gst.MESSAGE_EOS:
+        if t == gst.MESSAGE_EOS and not self.playlist.next():
             self.stop(save_resume_point=False)
-            if self.playlist.next():
-                self.play()
-            else:
-                self.notify( 'end_of_playlist', caller=self.__on_message )
+            self.notify( 'end_of_playlist', caller=self.__on_message )
 
         elif t == gst.MESSAGE_ERROR:
             err, debug = message.parse_error()

src/panucci/playlist.py
@@ -38,7 +38,7 @@ from services import ObservableService
 _ = lambda x: x
 
 class Playlist(ObservableService):
-    signals = [ 'new_track', 'file_queued' ]
+    signals = [ 'new_track', 'new_track_metadata', 'file_queued' ]
 
     def __init__(self):
         self.__log = logging.getLogger('panucci.playlist.Playlist')
@@ -118,8 +118,12 @@ class Playlist(ObservableService):
         return self.save_to_new_playlist(filepath)
 
     def on_queue_current_item_changed(self):
-        self.notify( 'new_track', self.get_file_metadata(),
-            caller=self.on_queue_current_item_changed )
+        self.send_new_metadata( self.on_queue_current_item_changed )
+        self.notify( 'new_track', caller=self.on_queue_current_item_changed )
+
+    def send_new_metadata(self, caller=None):
+        self.notify( 'new_track_metadata', self.get_file_metadata(),
+            caller=caller or self.send_new_metadata )
 
     def quit(self):
         self.__log.debug('quit() called.')
@@ -218,6 +222,9 @@ class Playlist(ObservableService):
             return False
 
         if bookmark_id is None:
+            if self.__queue.current_item_position == self.__queue.index(item_id):
+                self.next()
+
             item.delete_bookmark(None)
             self.__queue.remove(item_id)
         else:
@@ -331,12 +338,14 @@ class Playlist(ObservableService):
         else:                          # importing a single file
             error = not self.append(filepath, dont_notify=True)
 
+        self.__queue.disable_notifications = True
         self.load_from_resume_bookmark()
+        self.__queue.disable_notifications = False
 
         self.__queue.modified = os.path.expanduser(
             settings.temp_playlist ) == self.filepath
 
-        self.on_queue_current_item_changed()
+        self.send_new_metadata( caller=self.load )
 
         return not error
 
@@ -390,6 +399,8 @@ class Playlist(ObservableService):
         else:
             self.__log.warning('"%s" is not a directory.', directory)
             return False
+
+        return True
 
     ##################################
     # Playlist controls
@@ -471,6 +482,7 @@ class Queue(list, ObservableService):
 
         self.playlist_id = playlist_id
         self.modified = False # Has the queue been modified?
+        self.disable_notifications = False
         self.__current_item_position = 0
         list.__init__(self)
 
@@ -487,8 +499,9 @@ class Queue(list, ObservableService):
         if old_value != new_value:
             self.__log.debug( 'Current item changed from %d to %d',
                 old_value, new_value )
-            self.notify( 'current_item_changed',
-                caller=self.__set__current_item_position )
+            if not self.disable_notifications:
+                self.notify( 'current_item_changed',
+                    caller=self.__set__current_item_position )
 
     current_item_position = property(
         __get_current_item_position, __set__current_item_position )

src/panucci/panucci.py
@@ -337,9 +337,7 @@ class BookmarksWindow(gtk.Window):
     def jump_bookmark(self, w):
         model, bkmk_id, bkmk_iter, item_id, item_iter = self.__cur_selection()
         if item_iter is not None:
-            player.stop()
             player.playlist.load_from_bookmark_id( item_id, bkmk_id )
-            player.play()
 
 class GTK_Main(object):
 
@@ -364,7 +362,7 @@ class GTK_Main(object):
         player.register( 'playing', self.on_player_playing )
         player.register( 'paused', self.on_player_paused )
         player.register( 'end_of_playlist', self.on_player_end_of_playlist )
-        player.playlist.register( 'new_track', self.on_player_new_track )
+        player.playlist.register('new_track_metadata',self.on_player_new_track)
         player.playlist.register( 'file_queued', self.on_file_queued )
         player.init()
 
@@ -782,6 +780,7 @@ class GTK_Main(object):
     def on_player_playing(self):
         self.start_progress_timer()
         image(self.play_pause_button, 'media-playback-pause.png')
+        self.set_controls_sensitivity(True)
 
     def on_player_new_track(self, metadata):
         image(self.play_pause_button, 'media-playback-start.png')
@@ -789,7 +788,6 @@ class GTK_Main(object):
         self.button_handler_id = self.play_pause_button.connect(
             'clicked', self.on_btn_play_pause_clicked )
 
-        self.set_controls_sensitivity(True)
         for widget in [self.title_label,self.artist_label,self.album_label]:
             widget.set_text('')
             widget.hide()

commit 1b80e66e5299c9f9b6026c24a78c5fe297575277

Bugfix: Jumping to bookmarks in the current track
Regression from ee04fd56dd903312ecfccdb565f0499c2d4a9d8f

This method is better because it doesn't reload the track when jumping to
  bookmarks in the same file, it just seeks to the right point.

src/panucci/player.py
@@ -43,6 +43,7 @@ class panucciPlayer(ObservableService):
 
         self.playlist = Playlist()
         self.playlist.register( 'new_track', self.on_new_track )
+        self.playlist.register( 'seek-requested', self.do_seek )
         settings.register( 'volume_changed', self.__set_volume_level )
 
         self.__initial_seek = False # have we preformed the initial seek?

src/panucci/playlist.py
@@ -36,7 +36,7 @@ _ = lambda x: x
 
 class Playlist(ObservableService):
     signals = [ 'new_track', 'new_track_metadata', 'file_queued',
-        'bookmark_added' ]
+        'bookmark_added', 'seek-requested' ]
 
     def __init__(self):
         self.__log = logging.getLogger('panucci.playlist.Playlist')
@@ -134,12 +134,19 @@ class Playlist(ObservableService):
     ######################################
 
     def __load_from_bookmark( self, item_id, bookmark ):
-        self.__queue.current_item_position = self.__queue.index(item_id)
+        new_pos = self.__queue.index(item_id)
+        same_pos = self.__queue.current_item_position == new_pos
+        self.__queue.current_item_position = new_pos
 
         if bookmark is None:
             self.__queue.current_item.seek_to = 0
         else:
             self.__queue.current_item.seek_to = bookmark.seek_position
+
+            # if we don't request a seek nothing will happen
+            if same_pos:
+                self.notify( 'seek-requested', bookmark.seek_position,
+                    caller=self.__load_from_bookmark )
 
         return True
 