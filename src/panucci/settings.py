7f5d58761bba73b07e1be6124d2675357324c7f0
Nick Nobody <Nick Nobody me@nikosapi.org>
2010-01-05 19:12:40 -0500

    Fix copyright notice for 2010

--- src/panucci/settings.py
+++ src/panucci/settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 # This file is part of Panucci.
-# Copyright (c) 2008-2009 The Panucci Audiobook and Podcast Player Project
+# Copyright (c) 2008-2010 The Panucci Audiobook and Podcast Player Project
 #
 # Panucci is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
