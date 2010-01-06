22f53582cecc1a6982b1d55ebf994d1e0f8e14e4
Nick Nobody <Nick Nobody me@nikosapi.org>
2010-01-06 18:08:45 -0500

    Don't try running the headphone service on maemo 5

--- src/panucci/services.py
+++ src/panucci/services.py
@@ -104,7 +104,7 @@ class __headphone_watcher(ObservableService):
 
         self.__is_connected = None
 
-        if util.platform.MAEMO:
+        if util.platform.MAEMO and not util.platform.MAEMO5:
             try:
                 self.__sys_file = open( sys_file, 'r' )
                 self.__is_connected = self.__get_state_from_fd(self.__sys_file)
