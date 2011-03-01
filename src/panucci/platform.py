4b65b13e22a04f1be51db0631074a2581ed02e72
Thomas Perl <m@thp.io>
2011-03-01 21:49:14 +0100

    Update copyright years; add README file

--- src/panucci/platform.py
+++ src/panucci/platform.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Panucci.
-# Copyright (c) 2008-2010 The Panucci Audiobook and Podcast Player Project
+# Copyright (c) 2008-2011 The Panucci Project
 #
 # Panucci is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
