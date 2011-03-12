d2e61b881848c5fc4b621f15966fb9d2cdea3188
Jens Persson <xerxes2@gmail.com>
2011-03-12 16:49:34 +0100

    added about.py

---
+++ src/panucci/about.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Panucci.
+# Copyright (c) 2008-2011 The Panucci Project
+#
+# Panucci is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Panucci is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Panucci.  If not, see <http://www.gnu.org/licenses/>.
+
+about_name = 'Panucci'
+about_text = _('Resuming audiobook and podcast player')
+about_authors = ['Thomas Perl', 'Nick (nikosapi)', 'Matthew Taylor', 'Jens Persson']
+about_website = 'http://gpodder.org/panucci/'
+about_bugtracker = 'http://bugs.maemo.org/enter_bug.cgi?product=Panucci'
+about_donate = 'http://gpodder.org/donate'
+about_copyright = 'Copyright (c) 2008-2011 The Panucci Project'
