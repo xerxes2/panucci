6178b287ee0771e414c8d4af8e20dee99029a2bd
Jens Persson <xerxes2@gmail.com>
2011-03-24 01:56:07 +0100

    fixed save button for qt settings

--- src/panucci/qtui/qtsettingsdialog.py
+++ src/panucci/qtui/qtsettingsdialog.py
@@ -42,17 +42,17 @@ class SettingsDialog:
         # It does not seem to be possible to use action as proxies.
         self.box_scrolling_labels = QtGui.QCheckBox(_("Scrolling Labels").decode("utf-8"))
         self.box_scrolling_labels.setChecked(self.main.action_scrolling_labels.isChecked())
-        self.box_scrolling_labels.stateChanged.connect(self.scrolling_labels_callback)
+        #self.box_scrolling_labels.stateChanged.connect(self.scrolling_labels_callback)
         main_layout.addWidget(self.box_scrolling_labels)
 
         self.box_lock_progress = QtGui.QCheckBox(_("Lock Progress Bar").decode("utf-8"))
         self.box_lock_progress.setChecked(self.main.action_lock_progress.isChecked())
-        self.box_lock_progress.stateChanged.connect(self.lock_progress_callback)
+        #self.box_lock_progress.stateChanged.connect(self.lock_progress_callback)
         main_layout.addWidget(self.box_lock_progress)
 
         self.box_dual_action = QtGui.QCheckBox(_("Dual Action Button").decode("utf-8"))
         self.box_dual_action.setChecked(self.main.action_dual_action.isChecked())
-        self.box_dual_action.stateChanged.connect(self.dual_action_callback)
+        #self.box_dual_action.stateChanged.connect(self.dual_action_callback)
         main_layout.addWidget(self.box_dual_action)
 
         main_layout.addSpacing(10)
@@ -61,12 +61,12 @@ class SettingsDialog:
 
         self.box_stay_at_end = QtGui.QCheckBox(_("Stay at End").decode("utf-8"))
         self.box_stay_at_end.setChecked(self.main.action_stay_at_end.isChecked())
-        self.box_stay_at_end.stateChanged.connect(self.stay_at_end_callback)
+        #self.box_stay_at_end.stateChanged.connect(self.stay_at_end_callback)
         main_layout.addWidget(self.box_stay_at_end)
 
         self.box_seek_back = QtGui.QCheckBox(_("Seek Back").decode("utf-8"))
         self.box_seek_back.setChecked(self.main.action_seek_back.isChecked())
-        self.box_seek_back.stateChanged.connect(self.seek_back_callback)
+        #self.box_seek_back.stateChanged.connect(self.seek_back_callback)
         main_layout.addWidget(self.box_seek_back)
 
         main_layout.addSpacing(10)
@@ -76,41 +76,56 @@ class SettingsDialog:
 
         self.radio_play_mode_all = QtGui.QRadioButton(_("All").decode("utf-8"))
         self.radio_play_mode_all.setChecked(self.main.action_play_mode_all.isChecked())
-        self.radio_play_mode_all.toggled.connect(self.play_mode_all_callback)
+        #self.radio_play_mode_all.toggled.connect(self.play_mode_all_callback)
         group_play_mode.addButton(self.radio_play_mode_all)
         main_layout.addWidget(self.radio_play_mode_all)
 
         self.radio_play_mode_single = QtGui.QRadioButton(_("Single").decode("utf-8"))
         self.radio_play_mode_single.setChecked(self.main.action_play_mode_single.isChecked())
-        self.radio_play_mode_single.toggled.connect(self.play_mode_single_callback)
+        #self.radio_play_mode_single.toggled.connect(self.play_mode_single_callback)
         group_play_mode.addButton(self.radio_play_mode_single)
         main_layout.addWidget(self.radio_play_mode_single)
 
         self.radio_play_mode_random = QtGui.QRadioButton(_("Random").decode("utf-8"))
         self.radio_play_mode_random.setChecked(self.main.action_play_mode_random.isChecked())
-        self.radio_play_mode_random.toggled.connect(self.play_mode_random_callback)
+        #self.radio_play_mode_random.toggled.connect(self.play_mode_random_callback)
         group_play_mode.addButton(self.radio_play_mode_random)
         main_layout.addWidget(self.radio_play_mode_random)
 
         self.radio_play_mode_repeat = QtGui.QRadioButton(_("Repeat").decode("utf-8"))
         self.radio_play_mode_repeat.setChecked(self.main.action_play_mode_repeat.isChecked())
-        self.radio_play_mode_repeat.toggled.connect(self.play_mode_repeat_callback)
+        #self.radio_play_mode_repeat.toggled.connect(self.play_mode_repeat_callback)
         group_play_mode.addButton(self.radio_play_mode_repeat)
         main_layout.addWidget(self.radio_play_mode_repeat)
 
         layout = QtGui.QHBoxLayout()
         layout.addStretch(2)
-        button = QtGui.QPushButton(_("Close").decode("utf-8"))
-        button.clicked.connect(self.close)
+        button = QtGui.QPushButton(_("Save").decode("utf-8"))
+        button.clicked.connect(self.save)
         layout.addWidget(button)
         main_layout.addLayout(layout)
 
+        self.global_actions = [self.main.action_scrolling_labels, self.main.action_lock_progress,
+            self.main.action_dual_action, self.main.action_stay_at_end, self.main.action_seek_back,
+            self.main.action_play_mode_all, self.main.action_play_mode_single,
+            self.main.action_play_mode_random, self.main.action_play_mode_repeat]
+        self.local_actions = [self.box_scrolling_labels, self.box_lock_progress,
+            self.box_dual_action, self.box_stay_at_end, self.box_seek_back,
+            self.radio_play_mode_all, self.radio_play_mode_single,
+            self.radio_play_mode_random, self.radio_play_mode_repeat]
+        
         scrollarea.setWidget(main_widget)
         self.dialog.exec_()
 
     def close(self):
         self.dialog.close()
 
+    def save(self):
+        self.close()
+        for i in range(len(self.global_actions)):
+            self.global_actions[i].setChecked(self.local_actions[i].isChecked())
+
+    """
     def scrolling_labels_callback(self):
         self.main.action_scrolling_labels.setChecked(self.box_scrolling_labels.isChecked())
 
@@ -137,3 +152,4 @@ class SettingsDialog:
 
     def play_mode_repeat_callback(self):
         self.main.action_play_mode_repeat.setChecked(self.radio_play_mode_repeat.isChecked())
+    """
